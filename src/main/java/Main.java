2b7b70d9b289b8e9f7bacdf5df7e747a035e4b8d
Ethan Fine <ethanfine@umass.edu>
2016-10-30 02:15:59 -0400

    Static file demonstration.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+    	port(50001);
+    	staticFiles.location("/public");
         get("/hello", (req, res) -> "Hello World");
     }
 }
