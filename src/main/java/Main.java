bdf2529a4d68b27f70b64eca9ddd39eb2186e935
David Dzgoev <13dzgoevd@gmail.com>
2016-11-01 15:12:10 -0400

    Added additional simple paths to main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,36 @@ public class Main {
     	port(50001);
     	staticFiles.location("/public");
         get("/hello", (req, res) -> "Hello World");
+      
+        
+        post("/hello", (request, response) ->
+        "Hello World: " + request.body()
+    );
+
+    get("/private", (request, response) -> {
+        response.status(401);
+        return "Go Away!!!";
+    });
+
+    get("/users/:name", (request, response) -> "Selected user: " + request.params(":name"));
+
+    get("/news/:section", (request, response) -> {
+        response.type("text/xml");
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><news>" + request.params("section") + "</news>";
+    });
+
+    get("/protected", (request, response) -> {
+        halt(403, "I don't think so!!!");
+        return null;
+    });
+
+    get("/redirect", (request, response) -> {
+        response.redirect("/news/world");
+        return null;
+    });
+
+    get("/", (request, response) -> "root");
+        
+        
     }
 }
