6988bf2b5e16dd6708b00c66f511cdb66439858d
Manny <mannya897@gmail.com>
2016-11-08 20:55:02 -0500

    Completed documentation for idal and macro interfaces

--- src/main/java/bluemoose/idal/IDALInterface.java
+++ src/main/java/bluemoose/idal/IDALInterface.java
@@ -12,88 +12,88 @@ import java.util.List;
  */
 public interface IDALInterface {
 	
-	/*
+	/**
 	 * Returns all macros pending for peer review.
 	 *
-	 * @return	List of macros pending to peer reviewed
+	 * @return List of macros pending to peer reviewed
 	 */
 	public List<Macro> getAllPendingMacros();
 	
-	/*
+	/**
 	 * Returns a macro pending for peer review.
 	 *
-	 * @param 	ID 	macro identification number
-	 * @return		the specified macro
+	 * @param ID macro identification number
+	 * @return the specified macro
 	 */
 	public MacroInterface getPendingMacro(String ID);
 	
-	/*
+	/**
 	 * Persist macro, return formatted data, including generated macro unique id.
 	 *
-	 * @param 	creatorFname	first name of macro creator
-	 * @param 	creatorLname	last name of macro creator
-	 * @param 	macrotype		type of the macro
-	 * @parm 	parameters		list of parameters passed to macro
-	 * @return 					the built macro
+	 * @param creatorFname first name of macro creator
+	 * @param creatorLname last name of macro creator
+	 * @param macroType type of the macro
+	 * @param parameters list of parameters passed to macro
+	 * @return the built macro
 	 */
 	public MacroInterface storeMacro(String creatorFname, String creatorLname, String macroType, List<String> parameters);
 	
-	/*
+	/**
 	 * Persist a peer review of a macro, return formatted data.
 	 *
-	 * @param 	ID				macro identification number
-	 * @param 	reviewerLname	last name of macro reviewer
-	 * @param 	reviewerFname	first name of macro reviewer
-	 * @param 	parameters		list of parameters passed to macro
-	 * @return					the updated macro
+	 * @param ID macro identification number
+	 * @param reviewerLname last name of macro reviewer
+	 * @param reviewerFname first name of macro reviewer
+	 * @param parameters list of parameters passed to macro
+	 * @return the updated macro
 	 */
 	public MacroInterface reviewMacro(String ID, String reviewerLname, String reviewerFname, List<String> parameters);
 	
-	/*
+	/**
 	 * Moves a pending macro or peer reviewed macro to past macros.
 	 *
-	 * @param 	ID	macro identification number
-	 * @return 		the updated macro
+	 * @param ID macro identification number
+	 * @return the updated macro
 	 */
 	public MacroInterface markRan(String ID);
 	
-	/*
+	/**
 	 * returns all past macros.
 	 *
-	 * @return 		list of previously run Macros
+	 * @return list of previously run Macros
 	 */
 	public List<Macro> getJournal();
 	
-	/*
+	/**
 	 * Returns past macro by time period
 	 *
-	 * @param 	period	range of time period
-	 * @return 			list of previously run macros within period
+	 * @param period range of time period
+	 * @return list of previously run macros within period
 	 */
 	public List<Macro> viewJournal(Period period);
 	
-	/*
+	/**
 	 * Marks a past macro as failed, with the given reason.
 	 * TODO: requires group discussion of system architecture
 	 *
-	 * @param 	ID		macro identification number
-	 * @param	cause	specified reason for macro failing
+	 * @param ID macro identification number
+	 * @param cause specified reason for macro failing
 	 */
 	public void markFailed(String ID, String cause);
 	
-	/*
+	/**
 	 * returns the latest failed macro
 	 * TODO requires group discussion of system architecture
 	 *
-	 * @return			a failed macro
+	 * @return a failed macro
 	 */
 	public FailedMacro getLatestFailure();
 	
-	/*
+	/**
 	 * Returns a list of failed macros.
 	 * TODO: requires group discussion of system architecture
 	 *
-	 * @return 			list of failed macros
+	 * @return list of failed macros
 	 */
 	public List<Macro> getFailures();
 }
--- src/main/java/bluemoose/idal/MacroInterface.java
+++ src/main/java/bluemoose/idal/MacroInterface.java
@@ -5,54 +5,176 @@ import java.util.List;
 
 public interface MacroInterface {
 
+	/**
+	 * Returns all the parameters of the macro.
+	 *
+	 * @return list of parameters for the macro
+	 */
 	List<String> getParameters();
 
+	/**
+	 * Sets the parameters of the macro.
+	 *
+	 * @param index		index of the parameter within list of parameters
+	 * @param parameter	macro parameter
+	 */
 	void setParameter(int index, String parameter);
 
+	/**
+	 * Returns the list of parameters that the macro was originally built with
+	 *
+	 * @return list of parameters
+     */
 	List<String> getOriginalParameters();
 
+	/**
+	 * Returns the type of the macro
+	 *
+	 * @return type of the macro
+     */
 	String getMacroType();
 
+	/**
+	 * Sets the type of the macro
+	 *
+	 * @param macroType type of the macro
+     */
 	void setMacroType(String macroType);
 
+	/**
+	 * Return the first name of the macro creator
+	 *
+	 * @return first name of macro creator
+     */
 	String getCreatorFname();
 
+	/**
+	 * Set the first name of the macro creator
+	 *
+	 * @param creatorFname first name of macro creator
+     */
 	void setCreatorFname(String creatorFname);
 
+	/**
+	 * Return the last name of the macro creator
+	 *
+	 * @return last name of macro creator
+     */
 	String getCreatorLname();
 
+	/**
+	 * Set the last name of the macro creator
+	 *
+	 * @param creatorLname last name of macro creator
+     */
 	void setCreatorLname(String creatorLname);
 
+	/**
+	 * Return first name of macro reviewer
+	 *
+	 * @return first name of macro reviewer
+     */
 	String getReviewerFname();
 
+	/**
+	 * Set the first name of macro reviewer
+	 *
+	 * @param reviewerFname first name of macro reviewer
+     */
 	void setReviewerFname(String reviewerFname);
 
+	/**
+	 * Return the last name of macro reviewer
+	 *
+	 * @return last name of macro reviewer
+     */
 	String getReviewerLname();
 
+	/**
+	 * Set the last name of macro reviewer
+	 *
+	 * @param reviewerLname last name of macro reviewer
+     */
 	void setReviewerLname(String reviewerLname);
 
+	/**
+	 * Returns boolean value as to whether or not the macro was peer reviewed
+	 *
+	 * @return whether or not macro was peer reviewed
+     */
 	boolean getWasPeerReviewed();
 
+	/**
+	 * Set the macro to whether or not it was peer reviewed
+	 *
+	 * @param wasPeerReviewed whether or not macro was peer reviewed
+     */
 	void setWasPeerReviewed(boolean wasPeerReviewed);
 
+	/**
+	 * Return the data that the macro was run
+	 *
+	 * @return macro run date
+     */
 	Date getRunDate();
 
+	/**
+	 * Set the date that the macro was run
+	 *
+	 * @param runDate macro run date
+     */
 	void setRunDate(Date runDate);
 
+	/**
+	 * Set the macro run date to the current date
+	 *
+	 */
 	void setRunDateNow();
 
+	/**
+	 * Return the date that the macro was created
+	 *
+	 * @return macro creation date
+     */
 	Date getCreationDate();
 
+	/**
+	 * Set the date that the macro was created
+	 *
+	 * @param creationDate macro creation date
+     */
 	void setCreationDate(Date creationDate);
 
+	/**
+	 * Set the macro creation date to the current date
+	 *
+	 */
 	void setCreationDateNow();
 
+	/**
+	 * Return the macro ID number
+	 *
+	 * @return macro ID number
+     */
 	String getUniqueID();
 
+	/**
+	 * Set the macro ID number
+	 * @param uniqueID macro ID number
+     */
 	void setUniqueID(String uniqueID);
 
+	/**
+	 * Return whether or not the macro failed
+	 *
+	 */
 	boolean getFailed();
 
+	/**
+	 * Return the reason for macro failure
+	 *
+	 * @return macro failure message
+     */
 	String getFailureReason();
 
 }
